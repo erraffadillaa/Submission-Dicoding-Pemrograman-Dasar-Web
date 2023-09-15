4b76c56d1d1f23437455a9c9159ae37081a709e7
erraffadillaa <errafanindityafadilla@gmail.com>
2023-09-15 21:15:42 +0700

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+var n = prompt("Alat musik apa yang kamu cari:");
+var c = confirm(
+  "Yeay, kamu pilih " +
+    n +
+    "! Itu pilihan yang bagus, Yuk melihat barang lebih banyak di Erra's Store"
+);
+if (c == true) {
+  alert("Silahkan pilih alat musik yang kamu suka");
+} else {
+  alert("Kenapa?!");
+}
